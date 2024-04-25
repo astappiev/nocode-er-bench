c84d254c5ed8b7d238b1339a1068196790b16c79
Oleh Astappiev <astappiev@l3s.de>
2024-04-25 17:01:04 +0200

    feat: add more datasets

--- webapp/pages/api/datasets.js
+++ webapp/pages/api/datasets.js
@@ -3,15 +3,17 @@
 export default function handler(req, res) {
   const datasets = [
     {code: 'd1_fodors_zagats', name: 'Fodors-Zagats (Restaurants)'},
-    {code: 'd2_abt_buy', name: 'Abt-Buy'},
+    {code: 'd2_abt_buy', name: 'Abt-Buy (Products)'},
     {code: 'd3_amazon_google', name: 'Amazon-Google (Products)'},
-    {code: 'd4_dblp_acm', name: 'DBLP-ACM'},
-    {code: 'd5_imdb_tmdb', name: 'IMDB-TMDB'},
-    {code: 'd6_imdb_tvdb', name: 'IMDB-TVDB'},
-    {code: 'd7_tmdb_tvdb', name: 'TMDB-TVDB'},
-    {code: 'd8_amazon_walmart', name: 'Amazon-Walmart'},
-    {code: 'd9_dblp_scholar', name: 'DBLP-Scholar'},
+    {code: 'd4_dblp_acm', name: 'DBLP-ACM (Citations)'},
+    {code: 'd5_imdb_tmdb', name: 'IMDB-TMDB (Movies)'},
+    {code: 'd6_imdb_tvdb', name: 'IMDB-TVDB (Movies)'},
+    {code: 'd7_tmdb_tvdb', name: 'TMDB-TVDB (Movies)'},
+    {code: 'd8_amazon_walmart', name: 'Amazon-Walmart (Electronics)'},
+    {code: 'd9_dblp_scholar', name: 'DBLP-Scholar (Citations)'},
     {code: 'd10_imdb_dbpedia', name: 'IMDB-DBPedia (Movies)'},
+    {code: 'd11_itunes_amazon', name: 'iTunes-Amazon (Music)'},
+    {code: 'd12_beeradvo_ratebeer', name: 'BeerAdvo-RateBeer (Beer)'},
   ];
 
   res.status(200).json(datasets);
